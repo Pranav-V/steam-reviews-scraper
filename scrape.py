b680a2661fada6d5da0cd3f1fff23417825d2556
Pranav <pvenkatesh3141@gmail.com>
2022-05-16 16:52:00 -0500

    quick change

--- scrape.py
+++ scrape.py
@@ -23,14 +23,14 @@ for i in range(2,100000):
     with urllib.request.urlopen("https://steamcommunity.com/app/" + appID + "/homecontent/?userreviewscursor=" + cursor + "%3D&userreviewsoffset=" + str((10 * i) - 10) + "&p=" + str(i) + "&workshopitemspage=" + str(i) + "&readytouseitemspage=" + str(i) + "&mtxitemspage=" + str(i) + "&itemspage=" + str(i) + "&screenshotspage=" + str(i) + "&videospage=" + str(i) + "&artpage=" + str(i) + "&allguidepage=" + str(i) + "&webguidepage=" + str(i) + "&integratedguidepage=" + str(i) + "&discussionspage=" + str(i) + "&numperpage=10&browsefilter=mostrecent&browsefilter=mostrecent&appid=570&appHubSubSection=10&appHubSubSection=10&l=english&filterLanguage=" + language + "&searchText=&maxInappropriateScore=50&forceanon=1") as url:
         data = url.read().decode()
         parsed_html = BeautifulSoup(data)
-        cursor = parsed_html.body.find('input', attrs={'name':'userreviewscursor'})
+        cursor = parsed_html.find('input', attrs={'name':'userreviewscursor'})
         cursor = str(cursor['value'])[0: len(cursor) - 1]
         if '+' in cursor:
             cursor = cursor.replace('+', '%2B')
         print("Curser Location:", cursor)
         print("Next Indice:", i + 1)
        
-        scraped_users = parsed_html.body.find_all('div', attrs={'class':'apphub_Card modalContentLink interactable'})
+        scraped_users = parsed_html.find_all('div', attrs={'class':'apphub_Card modalContentLink interactable'})
 
         for review in scraped_users:
             base_term = ''
